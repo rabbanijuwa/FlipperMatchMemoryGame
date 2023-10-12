94075839521ef92f0f724e74bf6e64585545a5b1
juwarabbani <rabbanijuwa@gmail.com>
2023-10-12 17:12:07 -0400

    memory game

--- MemoryGame.java
+++ MemoryGame.java
@@ -12,54 +12,34 @@ public class MemoryGame extends JFrame {
     private int numberOfMatches;
     private int firstCardIndex;
     private int secondCardIndex;
+    private int moves;
+    private Timer timer;
+    private int gridSize;
 
-    public MemoryGame() {
+
+    public MemoryGame(int gridSize) {
+        this.gridSize = this.gridSize;
         setTitle("Picture Memory Game");
         setSize(2000, 2000);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         imagePaths = new ArrayList<>();
-
-        imagePaths.add("avacado.png");
-        imagePaths.add("peach.png");
-        imagePaths.add("grapes.png");
-        imagePaths.add("pineapple.png");
-        imagePaths.add("watermelon.png");
-        imagePaths.add("orangejuice.png");
-        imagePaths.add("lemon.png");
-        imagePaths.add("banana.png");
-        imagePaths.add("apple.png");
-        imagePaths.add("strawberry.png");
-        imagePaths.add("mango.png");
-        imagePaths.add("orange.png");
-        imagePaths.add("avacado.png");
-        imagePaths.add("peach.png");
-        imagePaths.add("grapes.png");
-        imagePaths.add("pineapple.png");
-        imagePaths.add("watermelon.png");
-        imagePaths.add("orangejuice.png");
-        imagePaths.add("lemon.png");
-        imagePaths.add("banana.png");
-        imagePaths.add("apple.png");
-        imagePaths.add("strawberry.png");
-        imagePaths.add("mango.png");
-        imagePaths.add("orange.png");
-
         cardImages = new ArrayList<>();
-        for (String imagePath : imagePaths) {
-            cardImages.add("");
-        }
+        numberOfMatches = 0;
+        firstCardIndex = -1;
+        secondCardIndex = -1;
+        moves = 0;
 
-        Collections.shuffle(imagePaths);
-        Collections.shuffle(cardImages);
+        initializeImagePaths();
+        initializeCardImages();
 
-        JPanel cardPanel = new JPanel(new GridLayout(4, 6));
-        cardButtons = new JButton[24];
+        JPanel cardPanel = new JPanel(new GridLayout(this.gridSize / 6, 6));
+        cardButtons = new JButton[this.gridSize];
 
         for (int i = 0; i < cardButtons.length; i++) {
             final int index = i;
             cardButtons[i] = new JButton();
-            cardButtons[i].setIcon(new ImageIcon("nothing.png"));
+            cardButtons[i].setIcon(new ImageIcon("cardback.png"));
             cardButtons[i].addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
@@ -71,6 +51,50 @@ public class MemoryGame extends JFrame {
 
         add(cardPanel);
     }
+
+    private void initializeImagePaths() {
+        imagePaths.add("avacado.png");
+        imagePaths.add("peach.png");
+        imagePaths.add("grapes.png");
+        imagePaths.add("pineapple.png");
+        imagePaths.add("watermelon.png");
+        imagePaths.add("orangejuice.png");
+        imagePaths.add("lemon.png");
+        imagePaths.add("banana.png");
+        imagePaths.add("apple.png");
+        imagePaths.add("strawberry.png");
+        imagePaths.add("mango.png");
+        imagePaths.add("orange.png");
+        imagePaths.add("avacado.png");
+        imagePaths.add("peach.png");
+        imagePaths.add("grapes.png");
+        imagePaths.add("pineapple.png");
+        imagePaths.add("watermelon.png");
+        imagePaths.add("orangejuice.png");
+        imagePaths.add("lemon.png");
+        imagePaths.add("banana.png");
+        imagePaths.add("apple.png");
+        imagePaths.add("strawberry.png");
+        imagePaths.add("mango.png");
+        imagePaths.add("orange.png");
+        // Add your image paths here for the specified grid size
+        // You should ensure you have enough images for the selected grid size
+        // Example: imagePaths.add("image1.png");
+        // ...
+
+        // Shuffle the image paths
+        Collections.shuffle(imagePaths);
+        Collections.shuffle(cardImages);
+    }
+    private void initializeCardImages() {
+        // Initialize cardImages based on the grid size
+        for (int i = 0; i < gridSize; i++) {
+            cardImages.add("");
+        }
+        Collections.shuffle(cardImages);
+    }
+
+
     private void handleCardClick(int index) {
         if (cardButtons[index].getIcon() == null) {
             return; // Already matched card, do nothing
@@ -83,43 +107,58 @@ public class MemoryGame extends JFrame {
             secondCardIndex = index;
             cardButtons[secondCardIndex].setIcon(new ImageIcon(imagePaths.get(index)));
 
-            Timer timer = new Timer(1000, new ActionListener() {
+            // Increment the moves
+            moves++;
+
+            timer = new Timer(5000, new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    if (imagePaths.get(firstCardIndex).equals(imagePaths.get(secondCardIndex))) {
-                       // cardButtons[firstCardIndex].setIcon(null);
-                      //  cardButtons[secondCardIndex].setIcon(null);
-                        cardButtons[firstCardIndex].setIcon(new ImageIcon("blue.png"));
-                        cardButtons[secondCardIndex].setIcon(new ImageIcon("blue.png"));
-                        cardImages.set(firstCardIndex, null);
-                        cardImages.set(secondCardIndex, null);
-                        numberOfMatches++;
-
-                        if (numberOfMatches == imagePaths.size() / 2) {
-                            JOptionPane.showMessageDialog(null, "Congratulations! You've won!");
-                            System.exit(0);
-                        }
-                    } else {
-                        cardButtons[firstCardIndex].setIcon(new ImageIcon("nothing.png"));
-                        cardButtons[secondCardIndex].setIcon(new ImageIcon("nothing.png"));
-                    }
+                    // Hide the cards after 5 seconds
+                    cardButtons[firstCardIndex].setIcon(new ImageIcon("cardback.png"));
+                    cardButtons[secondCardIndex].setIcon(new ImageIcon("cardback.png"));
                     firstCardIndex = -1;
+                    secondCardIndex = -1;
                 }
             });
             timer.setRepeats(false);
             timer.start();
+
+            if (imagePaths.get(firstCardIndex).equals(imagePaths.get(secondCardIndex))) {
+                cardButtons[firstCardIndex].setIcon(new ImageIcon("blue.png"));
+                cardButtons[secondCardIndex].setIcon(new ImageIcon("blue.png"));
+                cardImages.set(firstCardIndex, null);
+                cardImages.set(secondCardIndex, null);
+                numberOfMatches++;
+
+                if (numberOfMatches == gridSize / 2) {
+                    JOptionPane.showMessageDialog(null, "Congratulations! You've won in " + moves + " moves!");
+                    resetGame();
+                }
+            }
         }
     }
+    private void resetGame() {
+        // Reset the game by creating a new MemoryGame instance
+        // You can also provide an option for the user to start a new game
+        new MemoryGame(gridSize).setVisible(true);
+        dispose();
+    }
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
-
             @Override
             public void run() {
-                new MemoryGame().setVisible(true);
+                // Prompt the user for the grid size (12 cards or 24 cards)
+                String gridSizeInput = JOptionPane.showInputDialog("Enter the grid size (12 or 24):");
+                int gridSize = Integer.parseInt(gridSizeInput);
+
+                if (gridSize != 12 && gridSize != 24) {
+                    JOptionPane.showMessageDialog(null, "Invalid grid size. Please enter 12 or 24.");
+                    return;
+                }
+
+                new MemoryGame(gridSize).setVisible(true);
             }
         });
     }
-
-
 }
-
